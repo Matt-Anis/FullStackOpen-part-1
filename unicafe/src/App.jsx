a7ff65cc8d50ea68bf4a12c3dc905763315f1abe
Matt-Anis <webdev.omis@gmail.com>
2026-03-14 22:55:08 +0100

    1.7: unicafe step 2

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,26 +1,31 @@
-import  { useState } from 'react'
+import { useState } from "react";
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleClick = setReview => setReview(current => current + 1)  
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positive = all === 0 ? 0 : (good / all) * 100;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={() => handleClick(setGood)} text="good" />
-      <Button onClick={() => handleClick(setNeutral)} text="neutral" />
-      <Button onClick={() => handleClick(setBad)} text="bad" />
+      <Button onClick={() => setGood(g => g + 1)} text="good" />
+      <Button onClick={() => setNeutral(n => n + 1)} text="neutral" />
+      <Button onClick={() => setBad(b => b + 1)} text="bad" />
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
