3badee9088666b311deca5cbb129352280c32424
Matt-Anis <webdev.omis@gmail.com>
2026-03-14 23:27:39 +0100

    1.10: unicafe step 5

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const StatisticLine = ({ text, value, suffix }) => <p>{text} {value} {suffix}</p>
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
   const average = all === 0 ? 0 : (good - bad) / all;
   const positive = all === 0 ? 0 : (good / all) * 100;
 
-  if (all === 0) return <p>No feedback given</p>;
+  if (all === 0) return <StatisticLine text={'No feedback given'} />
   return (
     <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <StatisticLine text={'good'} value={good} />
+      <StatisticLine text={'neutral'} value={neutral} />
+      <StatisticLine text={'bad'} value={bad} />
+      <StatisticLine text={'all'} value={all} />
+      <StatisticLine text={'average'} value={average} />
+      <StatisticLine text={'positive'} value={positive} suffix={'%'} />
     </>
   );
 };
